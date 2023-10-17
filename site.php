5f5ae083863a6fedf50bab08be4c93ef95c94d6d
Matheus Vinicius <112036001+Mattttheus@users.noreply.github.com>
2023-10-17 20:04:57 -0300

    Add files via upload

---
+++ site.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+    <title>Ambiente bootstrap</title>
+    <link rel="stylesheet" type="text/css" href="stile.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+
+
+<body>
+    <class="row">
+        <div class="col-sm-3 logo"> logo
+
+        </div>
+
+        <div class="col-sm-9 menu"> menu
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8 C1"> C1 </div>
+            <div class="col-sm-5 C2"> C2 </div>
+            <div class="col-sm-3 C3 "> C3 </div>
+            <div class="col-sm-2 C4"> C4 </div>
+            <div class="col-sm-5C5"> C5 </div>
+            <div class="col-sm-6 C6"> C6</div>
+            <div class="col-sm-7 C7"> C7 </div>
+            <div class="col-sm-4 C8"> C8 </div>
+            <div class=" col-sm-9 C9"> C9 </div>
+            <div class="col-sm-8 C10 "> C10 </div>
+        </div>
+
+
+        <script src=" js/bootstrap.min.js">
+        </script>
+</body>
+
+
+</html>
